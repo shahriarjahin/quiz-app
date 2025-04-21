69fabbb04c225f90ac1d0b96a52d2c4e0aaa5b71
Shahriar Morshed Jahin <57398824+SHAHRIARJAHIN@users.noreply.github.com>
2025-04-22 00:29:52 +0600

    update

--- src/components/QuizInterface.js
+++ src/components/QuizInterface.js
@@ -1,6 +1,7 @@
 // components/QuizInterface.js
 import React, { useState } from 'react';
 import './QuizInterface.css';
+import { supabase } from '../utils/supabase';
 
 function QuizInterface({ questions, onAnswerSelect, answers, timeElapsed, onSubmit }) {
   const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
